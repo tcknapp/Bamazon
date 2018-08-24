e1e0ff0c26b25c9ca6651940410373db707b486e
tcknapp <trevor.knapp.1991@gmail.com>
2018-08-23 18:39:33 -0700

    begin fxns

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,91 @@
 /*
 Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 The app should then prompt users with two messages.
-
-
-
-The first should ask them the ID of the product they would like to buy.
-The second message should ask how many units of the product they would like to buy.
-
-
-
-Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-
-
-If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-However, if your store does have enough of the product, you should fulfill the customer's order.
-
-
-This means updating the SQL database to reflect the remaining quantity.
-Once the update goes through, show the customer the total cost of their purchase.
 */
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//establish connection 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazonDB"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
+/*Start - Running this application will first display all of the items available for sale. 
+  Include the ids, names, and prices of products for sale.*/
+
+function start() {
+  connection.query("SELECT * FROM bamazonDB.products", function (err,results){
+    if (err) throw err;
+    console.log(" ")
+    console.log("==============*BAMAZON*=============")
+    console.log(" ")
+    console.log("--------PRODUCTS-------")
+
+    //display results in readable format 
+    for(var i = 0; i< results.length; i++){
+      console.log("ID: " + results[i].item_id + "|" + "Product: " + results[i].product_name + "|" 
+      + "Department: " + results[i].department_name + "|" + "Price: " + results[i].price + "|" 
+      + "Available: " + results[i].stock_quantity);
+    }
+
+    
+  })
+};
+start();
+  
+
+
+//The app should then prompt users with two messages...  
+// The first should ask them the ID of the product they would like to buy.
+function askId() {
+  inquirer
+    .prompt({
+      name: "askId",
+      type: "input",
+      message: "What is the [ID] of the product you would like to buy?",
+      //make sure id is valid
+      validate: (function(value) {
+        if(input(value) === true){
+          return true;
+        }
+        else {
+          console.log("Invalid [ID]");
+        }
+      }
+    )}
+  )};  
+  askId(); 
+
+
+
+// The second message should ask how many units of the product they would like to buy.
+
+
+
+
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
